43d09e41307359334bb4b0f2032f341b2c0a5f1e
hasan-dev <hasan01nov@gmail.com>
2023-10-19 21:27:36 +0700

    slicing detail mitra

--- lib/dummy.dart
+++ lib/dummy.dart
@@ -9,7 +9,22 @@ class Dummy extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Center(child: LaundryDetailChooser()),
+        child: Column(
+          children: <Widget>[
+            const Padding(padding: EdgeInsets.only(top: 70)),
+            Image.asset('lib/assets/images/rectangle.png'),
+            const Padding(padding: EdgeInsets.all(10)),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              style: TextStyle(
+                  color: Theme.of(context)
+                      .colorScheme
+                      .onBackground
+                      .withOpacity(0.5)),
+            ),
+            Center(child: LaundryDetailChooser()),
+          ],
+        ),
       ),
     );
   }
